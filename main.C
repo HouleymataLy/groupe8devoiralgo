ccd7adb002ef858122b2861bea1a40c83da18cce
aissatouAFI <maronea865@gmail.com>
2025-06-17 15:22:29 +0000

    Create main.C

---
+++ main.C
@@ -0,0 +1,11 @@
+//par Houleymata LY
+
+main(){
+    remplirtab(tab[N]);
+    affichetab(tab[N]);
+   sommetab(tab[N]);
+   mintab(tab[N]);
+    maxtab(tab[N]);
+ sommeparobjet(tab[N]);
+  factureparetudiant(tab[N]);
+}
