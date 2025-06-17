a15d231c143232c580a4edb390145e5114d48e85
lefa-04 <fallpapedjiby68@gmail.com>
2025-06-17 15:10:31 +0000

    Create fonctionmaxtab.c

---
+++ fonctionmaxtab.c
@@ -0,0 +1,11 @@
+float maxtab(struct facture tab[N]) {
+    int i;
+    float max = tab[0].montant;
+    for (i = 0; i < N; i++) {
+        if (tab[i].montant > max) {
+            max = tab[i].montant;
+        }
+    }
+    printf("Le montant maximum est : %f\n", max);
+    return max;
+}
